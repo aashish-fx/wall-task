7b209d72eb3cadd04f23163dc9677f53045f4ac4
aashish <ash569sharma@gmail.com>
2022-10-27 17:23:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Ens Linked';
+  const sub_title = 'Ens linked'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App_container'>
+        <div className="App">
+              <div className='upperpart'>
+                  <div className='ens_image'>
+                  </div>
+                  <div>
+                    <div className='ens_heading'>
+                      <span className='ens_heading_span1'>{title}</span>
+                      <span className='ens_heading_span2'>{sub_title}</span>
+                    </div>
+                  </div>
+              </div>
+              <div className='lowerpart'>
+                  <div className='lowerpart_div1'>
+                    <span className='lowerpart_span1'>dingaing.eth</span>
+                    <div className='lowerpart_inner_div1'>
+                      <span className='lowerpart_span2'>1.82</span>
+                      <div className='lowerpart_image'></div>
+                    </div>
+                  </div>
+                  <div className='lowerpart_div2'>
+                    <span className = 'lowerpart_div2_span1'>2 months ago</span>
+                    <span className = 'lowerpart_div2_span2'>+200xp</span>
+                  </div>
+              </div>
+
+    </div>
     </div>
   );
 }
